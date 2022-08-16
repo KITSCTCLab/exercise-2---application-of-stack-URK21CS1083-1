4d6f9d629246f5660b0a753377c09274909817a8
URK21CS1083 <110010876+URK21CS1083@users.noreply.github.com>
2022-08-16 11:55:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import operator
 class Evaluate:
   """This class validates and evaluate postfix expression.
   Attributes:
@@ -6,8 +7,8 @@ class Evaluate:
       stack: A List which acts as a Stack.
   """
     # Write your code here
-
-
+ 
+ 
   def __init__(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
@@ -16,8 +17,8 @@ class Evaluate:
     self.top = -1
     self.size_of_stack = size
     self.stack = []
-
-
+ 
+ 
   def isEmpty(self):
     """
     Check whether the stack is empty.
@@ -25,17 +26,25 @@ class Evaluate:
       True if it is empty, else returns False.
     """
       # Write your code here
-
-
-  def pop(self):
+    if len(self.stack)==0:
+        return True
+    else:
+        return False
+ 
+ 
+  def _pop(self):
     """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
     # Write your code here
-
-
+    if self.isEmpty():
+      print("Your stack is empty")
+    else:
+        return self.stack.pop()
+ 
+ 
   def push(self, operand):
     """
     Push the operand to stack if the stack is not full.
@@ -43,8 +52,15 @@ class Evaluate:
       operand: The operand to be pushed.
     """
     # Write your code here
-
-
+    self.operand = operand
+    if len(self.stack)==self.size_of_stack:
+      print("Your stack is full")
+    else:
+        self.stack.append(self.operand)
+        self.top+=1
+       
+ 
+ 
   def validate_postfix_expression(self, expression):
     """
     Check whether the expression is a valid postfix expression.
@@ -53,9 +69,17 @@ class Evaluate:
     Returns:
       True if the expression is valid, else returns False.
     """
-    # Write your code here
-
-
+    for x in expression:
+        if x.isnumeric()==True:
+            return True
+        else:
+            if x=="+" or x=="-" or x=="" or x=="/" or x=="*":
+                return True
+            else:
+                return False
+   
+ 
+ 
   def evaluate_postfix_expression(self, expression):
     """
     Evaluate the postfix expression
@@ -64,9 +88,18 @@ class Evaluate:
     Returns:
       The result of evaluated postfix expression.
     """
-    # Write your code here
-
-
+    for i in expression:
+        if i.isnumeric():
+            self.push(int(i))
+        else:
+           a=self._pop()
+           b=self._pop()
+           operations_dictionary={"+":operator.add,"-":operator.sub,"*":operator.mul,"/":operator.floordiv,"^":operator.pow}
+           self.push(operations_dictionary[i](b,a))
+     
+    return self.stack[0]
+ 
+ 
 # Do not change the following code
 postfix_expression = input()  # Read postfix expression
 tokens = postfix_expression.split()
